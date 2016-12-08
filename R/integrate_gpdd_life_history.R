aa8148eb84d3e8a296461c4cc551d778500bc95a
David M Anderson <daan4786@ufl.edu>
2016-12-08 11:06:43 -0500

    Added functions to easily integrate package with rgpdd.

---
+++ R/integrate_gpdd_life_history.R
@@ -0,0 +1,63 @@
+#' A function to integrate GPDD time series data with trait data.
+#'
+#' This function calls the taxonomic data table from rgpdd, and joins it with the life history data using the TaxonID index. 
+#' In addition, this function can be used to join to the GPDD main data table, which contains indexes for all other data tables.
+#' @param 
+#' taxon Do you want to include taxonomic information from the gpdd_taxon data table? Default is FALSE
+#' main Do you want to include information on time series from the gpdd_main data table? Default is TRUE.
+#' source Do you want to include information on data sources for the life history data? Default is FALSE.
+#' @examples data <- gpdd_taxon_life_history()
+#' head(data)
+#' data_taxon_sources <- gpdd_taxon_life_history(taxon=TRUE, main=FALSE, sources=TRUE)
+#' head(data_taxon_sources)
+
+gpdd_taxon_life_history <- function(taxon=FALSE, main=TRUE, sources=FALSE){
+	
+	if(taxon == TRUE){
+		
+		if(main == FALSE & sources == FALSE){
+		df <- dplyr::left_join(rgpdd::gpdd_taxon, rgpddPlusTraits::life_history, by = "TaxonID")
+		} else{
+		
+			if(main == TRUE & sources == TRUE){
+				df <- dplyr::left_join(rgpdd::gpdd_taxon, rgpddPlusTraits::life_history, by = "TaxonID")
+				df <- dplyr::left_join(df, rgpdd::gpdd_main, by = "TaxonID")
+				df <- dplyr::left_join(df, rgpddPlusTraits::life_history_sources, by = "TaxonID")
+			} else{	
+				if(main == TRUE & sources == FALSE){
+				df <- dplyr::left_join(rgpdd::gpdd_taxon, rgpddPlusTraits::life_history, by = "TaxonID")
+				df <- dplyr::left_join(df, rgpdd::gpdd_main, by = "TaxonID")
+				} else{
+				df <- dplyr::left_join(rgpdd::gpdd_taxon, rgpddPlusTraits::life_history, by = "TaxonID")
+				df <- dplyr::left_join(df, rgpddPlusTraits::life_history_sources, by = "TaxonID")
+				}
+			}			
+		} 
+				
+	} else{
+		
+		if(main == FALSE & sources == FALSE){
+		
+		df <- 0
+		
+		} else{
+			
+			if(main == TRUE & sources == TRUE){
+			df <- dplyr::left_join(rgpddPlusTraits::life_history, rgpdd::gpdd_main, by = "TaxonID")
+			df <- dplyr::left_join(df, rgpddPlusTraits::life_history_sources, by = "TaxonID")
+			} else{
+			
+				if(main == TRUE & sources == FALSE){
+				df <- dplyr::left_join(rgpddPlusTraits::life_history, rgpdd::gpdd_main, by = "TaxonID")
+				} else{
+				df <- dplyr::left_join(rgpddPlusTraits::life_history, rgpddPlusTraits::life_history_sources, by = "TaxonID")
+				}
+			
+			}
+			
+		} 
+			
+	}	
+		
+	return(df)
+}
---
+++ R/integrate_gpdd_temperature.R
@@ -0,0 +1,36 @@
+#' A function to integrate GPDD time series data with temperature data.
+#'
+#' This function calls the location data table from rgpdd, and joins it with the temperature data using the LocationID index. 
+#' In addition, this function can be used to join to the GPDD main data table, which contains indexes for all other data tables.
+#' @param 
+#' location Do you want to include information on location of the population from the gpdd_location data table? Default is FALSE.
+#' main Do you want to include information on time series from the gpdd_main data table? Default is TRUE.
+#' @examples 
+#' data <- gpdd_location_temperature() # only include location meta data
+#' head(data)
+#' data_main <- gpdd_location_temperature(location=TRUE, main=TRUE) #include both location meta data and main table.
+#' head(data_main)
+
+gpdd_location_temperature <- function(location=FALSE,main=TRUE){
+	
+	if(location == TRUE){
+		
+		if(main == FALSE){
+		df <- dplyr::left_join(rgpdd::gpdd_location, rgpddPlusTraits::temperature, by = "LocationID")
+		} else{
+		df <- dplyr::left_join(rgpddPlusTraits::temperature, rgpdd::gpdd_main, by = "LocationID")
+		df <- dplyr::left_join(rgpdd::gpdd_location, df, by = "LocationID")
+		}
+		
+	} else{
+		
+		if(main == FALSE){
+		df <- 0
+		} else{
+		df <- dplyr::left_join(rgpddPlusTraits::temperature, rgpdd::gpdd_main, by = "LocationID")
+		}
+		
+	}
+		
+	return(df)
+}
